aa249acb6a0f2de34423fa400c001b1fa246eacd
Jayleen Li <jayleenli17@gmail.com>
2020-04-25 01:56:30 -0500

    Updates

---
+++ precache-manifest.84f962852bd1837458ba47643a1a7ee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa16cff5e04d2680a3c6b64aaab9eb9c",
+    "url": "/cs185-site/index.html"
+  },
+  {
+    "revision": "c0c8f5db7ff9a5ff7477",
+    "url": "/cs185-site/static/css/main.1dd839a9.chunk.css"
+  },
+  {
+    "revision": "5f353b10e578a733f2b9",
+    "url": "/cs185-site/static/js/2.ff8bd651.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cs185-site/static/js/2.ff8bd651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0c8f5db7ff9a5ff7477",
+    "url": "/cs185-site/static/js/main.86397746.chunk.js"
+  },
+  {
+    "revision": "644345f4469d73c69328",
+    "url": "/cs185-site/static/js/runtime-main.4737224e.js"
+  }
+]);
