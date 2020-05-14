12552aef2399fcfb5622cd5f4aa44795cb1c4dd1
Jayleen Li <jayleenli17@gmail.com>
2020-05-14 16:14:09 -0500

    Wanted to remove my last name

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -10,7 +10,7 @@ export default class Home extends Component {
 			</div>
 			<div>
 				<p>
-					Hi my name is Jayleen Li and I am a junior at UC Santa Barbara. Since I already made a <a href="http://jayleenli.github.io" target="_blank">professional portfolio</a> I decided to have more fun with this portfolio for CS 185. :) 
+					Hi my name is Jayleen and I am a junior at UC Santa Barbara. Since I already made a <a href="http://jayleenli.github.io" target="_blank">professional portfolio</a> I decided to have more fun with this portfolio for CS 185. :) 
 					<br />
 					<br />
 					Outside of school/coding things I like playing video games, watching anime or YouTube animations, and obessessing over korean girl groups. 
